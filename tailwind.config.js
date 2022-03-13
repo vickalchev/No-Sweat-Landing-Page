5736241f7ebf3ab0ccbb0dccfc201dec761b61cc
Vic Kalchev <vic@pivotup.co>
2022-03-12 20:46:12 -0500

    Added various sections: Hero, We Are  and CTA1.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,32 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontSize: {
+        'xs': '.75rem',
+        'sm': '.875rem',
+        'tiny': '.875rem',
+        'base': '1rem',
+        'lg': '1.125rem',
+        'xl': '1.25rem',
+        '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '6rem',
+      },
+    fontFamily: {
+      Inter: ['Inter, san-serif'],
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xl: '1124px',
+        '2xl': '1124px',
+      },
+    },
   },
   plugins: [],
 }
